04a625f7e1f4160dcdb7ae3fd2a8b12981da66fa
miloshk <milos.kostadinovski97@gmail.com>
2017-07-29 01:03:23 +0200

    Added configuration file

---
+++ mbconf.py
@@ -0,0 +1,10 @@
+
+# MySQL configuration variables
+MYSQL_HOST = 'localhost'
+MYSQL_USER = 'username'
+MYSQL_PASSWORD = 'password'
+MYSQL_DB = 'database'
+
+
+#Add run arguments into [], splitted by comma.
+RUN_ARGS = []
