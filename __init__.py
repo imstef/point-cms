a822d3eaa22a56fafdebcf283b9eaeba7694c28c
imilosk <milos.kostadinovski97@gmail.com>
2017-08-01 00:22:13 +0200

    Finished login form, registration script and user sessions

--- __init__.py
+++ __init__.py
@@ -1,9 +1,11 @@
 
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, session, logging, redirect, flash
 from passlib.hash import sha256_crypt
 # files
 from dbhandler import DatabaseConnection
+from MySQLdb import escape_string as thwart
 from mbconf import *
+from functools import wraps
 
 app = Flask(__name__)
 
@@ -25,30 +27,71 @@ def homepage():
 
 # end def
 
+def is_logged_in(f):
+	@wraps(f)
+	def wrap(*args, **kwargs):
+		if 'logged_in' in session:
+			return f(*args, **kwargs)
+		else:
+			#flash()
+			return redirect(url_for('login'))
+	return wrap
+# end def
+
+def is_logged_out(f):
+	@wraps(f)
+	def wrap(*args, **kwargs):
+		if 'logged_in' not in session:
+			return f(*args, **kwargs)
+		else:
+			#flash()
+			return redirect(url_for('dashboard'))
+	return wrap
+# end def
+
 @app.route("/login/", methods=["GET", "POST"])
+@is_logged_out
 def login():
+	message = ""
 	if request.method == "POST":
 		username = request.form["username"]
 		password_candidate = request.form["password"]
 
 		if username and password_candidate:
 			connection.connect()
-			result = connection.execute_query("SELECT * FROM users WHERE username = '{}'".format(username))
-			connection.close()
+			result = connection.execute_query("SELECT * FROM users WHERE username = '{}'".format(thwart(username)))
 
-			if len(result) == 1 and password_candidate == result[0]["password"]:
-				return "Welcome!"
+			if len(result) == 1 and sha256_crypt.verify(password_candidate, result[0]["password"]):
+				connection.close()
+				session["logged_in"] = True
+				session["username"] = username
+				return redirect(url_for('dashboard'))
 			else:
-				return "Wrong username or password"
-
+				message = "Wrong username or password"
+				flash(message)
+				return render_template("login.html.j2")
 		else:
-			return "Enter username and password"
+			message = "Enter username and password"
+			flash(message)
+			return render_template("login.html.j2")
 
 	return render_template('login.html.j2')
-
 # end def
 
-# global variables that cna be accessed from Jinja templates
+@app.route("/dashboard/")
+@is_logged_in
+def dashboard():
+	return render_template("dashboard.html.j2")
+# end def
+
+@app.route("/logout/")
+@is_logged_in
+def logout():
+	session.clear()
+	return redirect(url_for('homepage'))
+# end def
+
+# global variables that can be accessed from Jinja templates
 app.jinja_env.globals.update(close=connection.close)
 app.jinja_env.globals.update(execute_query=connection.execute_query)
 app.jinja_env.add_extension('jinja2.ext.loopcontrols')
--- register.py
+++ register.py
@@ -1,9 +1,29 @@
 from passlib.hash import sha256_crypt
 import getpass
 from dbhandler import DatabaseConnection
+from MySQLdb import escape_string as thwart
 
 username = str(raw_input("Enter username: "))
 password = getpass.getpass("Enter password:")
+password2 = getpass.getpass("Confirm password:")
 
+if password != password2:
+	print("Passwords do not match!")
+	exit (2)
+
+password = sha256_crypt.encrypt(password)
 connection = DatabaseConnection()
+connection.connect()
+
+query = "SELECT * from users WHERE username = '{}'".format(thwart(username))
+data = connection.execute_query(query)
+if len(data) == 1:
+	print("Users exists!")
+	exit (1)
+
+query = "INSERT INTO users (username, password) VALUES('{}', '{}')".format(thwart(username), thwart(password))
+connection.execute_query(query)
+connection.execute_query("commit")
 connection.close()
+
+print("Done!")
