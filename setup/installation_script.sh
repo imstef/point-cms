217c4593715e19b5380cccb6517b70ed211b3c86
Milos Kostadinovski <milos.kostadinovski97@gmail.com>
2018-10-19 12:42:13 +0200

    Changed .sql script name
    Wrong .sql script name in installation file.

--- setup/installation_script.sh
+++ setup/installation_script.sh
@@ -89,7 +89,7 @@ application.secret_key = '$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 |
 (pip install flask-mysqldb)
 (pip install passlib)
 (mysql -u root -p -e ' DROP DATABASE IF EXISTS $app_name; CREATE DATABASE $app_name;')
-(mysql -u root -p $app_name < ./setup/installation_script.sql)
+(mysql -u root -p $app_name < ./setup/point.sql)
 (apt-get install python-mysqldb)
 (service --assume-yes apache2 restart)
 
